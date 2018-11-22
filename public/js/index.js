df3c317526c647b83078d98c804225789ab907da
Aryel Santos <aryel.ds@gmail.com>
2018-11-22 17:51:17 -0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function() {
     console.log('Connect to server');
-
-    socket.emit('createMessage', {
-        from: "Jen@example.com",
-        text: "Tudo bem??"
-    });
 });
 
 socket.on('disconnect', function() {
