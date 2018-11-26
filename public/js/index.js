922c972302c392bbf9d119580189b391f526353c
Aryel Santos <aryel.ds@gmail.com>
2018-11-26 17:43:28 -0200

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -25,14 +25,16 @@ socket.on('newLocationMessage', function (message) {
     $('#messages').append(li);
 });
 
+let messageTextBox = $('[name=message]');
+
 $("form#message-form").on('submit', function (e) {
     e.preventDefault();
 
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextBox.val()
     }, function () {
-
+        messageTextBox.val('');
     });
 });
 
@@ -42,13 +44,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position);
+        locationButton.removeAttr('disabled').text('Sending location');
         socket.emit('createLocationMessage', {
            latitude: position.coords.latitude,
            longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Sending location');
         alert('Unable to fetch location.');
     });
 });
